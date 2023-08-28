5c02d53a7ba4fe5d93e5bad4cd5f920c336a37b7
israa252 <99605048+israa252@users.noreply.github.com>
2023-08-28 11:18:32 +0300

    Update ProductsGrid.js

--- components/ProductsGrid.js
+++ components/ProductsGrid.js
@@ -21,10 +21,10 @@ const ProductImage = styled.img`
 export default function ProductsGrid({ products }) {
   // Settings for the react-slick slider
   const sliderSettings = {
-    
+    dots:true,
     speed: 900,
      autoplay: true, // Enable autoplay
-    autoplaySpeed: 4000, // Set autoplay interval in milliseconds (e.g., 3 seconds)
+    autoplaySpeed: 3000, // Set autoplay interval in milliseconds (e.g., 3 seconds)
     slidesToShow: 4, // Number of products to show per slide
     slidesToScroll: 1,
     responsive: [
